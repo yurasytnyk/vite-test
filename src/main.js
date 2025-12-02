a8094790cccc8001678ecff60833addb624273e7
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2025-12-02 20:25:47 +0200

    test

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ document.querySelector('#app').innerHTML = `
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
